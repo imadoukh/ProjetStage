d834e2c9825571c2f5a1796dbd2d43ddfb694586
imadOukhou <oukhouch.imad@gmail.com>
2024-12-25 13:09:34 +0100

    ajout  de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"user",component:AppComponent},
+  {path:"register",component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
